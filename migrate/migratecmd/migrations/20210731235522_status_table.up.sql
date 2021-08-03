0b5ec7126d98a7174c999b873e5a941401192e19
frantjc <logsdonalex11@gmail.com>
2021-08-02 22:46:46 -0400

    add status table migration

---
+++ migrate/migratecmd/migrations/20210731235522_status_table.up.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS status_type ( 
+    job_status VARCHAR (32) PRIMARY KEY
+);
--- migrate/migratecmd/migrations/20210731235523_job_table.up.sql
+++ migrate/migratecmd/migrations/20210731235523_job_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS job ( 
     job_id VARCHAR (64) PRIMARY KEY,
     task_type VARCHAR (32) NOT NULL REFERENCES task(task_type),
-    job_status VARCHAR (32) NOT NULL,
+    job_status VARCHAR (32) NOT NULL REFERENCES status_type(job_status),
     job_error VARCHAR (128)
 );
