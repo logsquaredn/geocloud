dd6ad9311a6d86124e4a6dfe5797a92946986b8c
phish3y <logsdonevan1@gmail.com>
2021-10-28 20:52:02 -0700

    task cleanup

--- tasks/filter/filter.c
+++ tasks/filter/filter.c
@@ -19,8 +19,8 @@ int main(int argc, char *argv[]) {
 	const char *filterValue = argv[4];
 	fprintf(stdout, "filter value: %s\n", filterValue);
 
-	char *inputGeoFilePath;
-	if(getInputGeoFilePath(inputFilePath, &inputGeoFilePath)) {
+	char *inputGeoFilePath = getInputGeoFilePath(inputFilePath);
+	if(inputGeoFilePath == NULL) {
 		error("failed to find input geo file path", __FILE__, __LINE__);
 	}
 	fprintf(stdout, "input geo file path: %s\n", inputGeoFilePath);
