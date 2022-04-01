28e92994da7ff7585e963ff6bf40652c05c8f2c0
phish3y <logsdonevan1@gmail.com>
2022-04-01 12:58:50 -0700

    Janitor cleaning up minio and postgres

--- janitor/src/main.rs
+++ janitor/src/main.rs
@@ -1,16 +1,48 @@
-extern crate postgres;
+use chrono::{DateTime, Duration, Utc};
+use s3::bucket::Bucket;
+use s3::creds::Credentials;
+use s3::region:: Region;
 
-use chrono::{DateTime, Utc};
-use postgres::{Client, NoTls, Error};
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (postgres_client, connection): (tokio_postgres::Client, _) = tokio_postgres::connect("postgres://geocloud:geocloud@localhost:5432?sslmode=disable", tokio_postgres::NoTls).await?;
 
-fn main() -> Result<(), Error> {
-    let mut client = Client::connect("postgres://geocloud:geocloud@localhost:5432?sslmode=disable", NoTls)?;
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            panic!("connection error: {}", e);
+        }
+    });
 
-    for row in client.query("SELECT * FROM job", &[])? {
+    let bucket: Bucket = Bucket::new_with_path_style(
+        "geocloud", 
+        Region::Custom {
+            region: "".to_owned(),
+            endpoint: "http://127.0.0.1:9000".to_owned()
+        }, 
+        Credentials::from_profile(Some("default"))?
+    )?;
+
+    let delete_before: DateTime<Utc> = Utc::now() - Duration::minutes(1); // Duration::days(14);
+    println!("Cleaning up jobs before: {:?}", delete_before);
+    for row in postgres_client.query("SELECT * FROM job", &[]).await? {
         let id: &str = row.try_get("job_id")?;
         let end_time: DateTime<Utc> = row.try_get("end_time")?;
-        println!("{}", id);
-        println!("{:?}", end_time);
+
+        if delete_before > end_time {
+            println!("Cleaning up Job ID: {}", id);
+            let mut key = "jobs/".to_owned();
+            key.push_str(id);
+            let results = bucket.list(key.clone(), None).await?;
+            for result in results {
+                for item in result.contents {
+                    bucket.delete_object(item.key).await?;
+                }
+            }
+
+            bucket.delete_object(key).await?;
+
+            postgres_client.execute("DELETE FROM job WHERE job_id = $1", &[&id]).await?;
+        }
     }
 
     Ok(())
