5c18e96815087b014e5559d52406aa578fb60eb5
phish3y <logsdonevan1@gmail.com>
2022-04-28 18:57:30 -0700

    Add cli configuration to janitor

--- janitor/src/main.rs
+++ janitor/src/main.rs
@@ -4,10 +4,44 @@ use s3::creds::Credentials;
 use s3::region:: Region;
 use std::io::{Cursor, Write, Seek, SeekFrom};
 use std::time::SystemTime;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Args {
+
+    #[clap(short, long, default_value_t = 1440)]
+    delete_before: i64,
+
+    #[clap(short, long, default_value = "localhost:5432")]
+    addr: String,
+
+    #[clap(short, long, default_value = "geocloud")]
+    user: String,
+
+    #[clap(short, long)]
+    password: String,
+
+    #[clap(short, long, default_value = "disable")]
+    sslmode: String,
+
+    #[clap(short = 'r', long, default_value = "geocloud-archive")]
+    archive_bucket: String,
+
+    #[clap(short, long, default_value = "geocloud")]
+    minio_bucket: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (postgres_client, connection): (tokio_postgres::Client, _) = tokio_postgres::connect("postgres://geocloud:geocloud@localhost:5432?sslmode=disable", tokio_postgres::NoTls).await?;
+    let args = Args::parse();
+
+    let user = args.user;
+    let password = args.password;
+    let addr = args.addr;
+    let sslmode = args.sslmode;
+    let conn_string = format!("postgres://{user}:{password}@{addr}?sslmode={sslmode}");
+    let (postgres_client, connection): (tokio_postgres::Client, _) = tokio_postgres::connect(&conn_string, tokio_postgres::NoTls).await?;
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
@@ -16,7 +50,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
     let minio_bucket: Bucket = Bucket::new_with_path_style(
-        "geocloud", 
+        &args.minio_bucket, 
         Region::Custom {
             region: "".to_owned(),
             endpoint: "http://127.0.0.1:9000".to_owned()
@@ -24,16 +58,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Credentials::from_profile(Some("default"))?
     )?;
 
-    let s3_bucket: Bucket = Bucket::new(
-        "v0-1-2-geocloud", 
-        "".parse()?, 
+    let archive_bucket: Bucket = Bucket::new(
+        &args.archive_bucket, 
+        "us-east-1".parse()?, 
         Credentials::from_profile(Some("default"))?
     )?;
 
     let mut archive = Cursor::new(Vec::new());
     archive.write_all("job_id,task_type,job_status,job_error,start_time,end_time,job_args\n".as_bytes())?;
    
-    let delete_before: DateTime<Utc> = Utc::now() - Duration::minutes(1); // Duration::days(14);
+    let delete_before: DateTime<Utc> = Utc::now() - Duration::minutes(args.delete_before); // Duration::days(14);
     println!("Cleaning up jobs before: {:?}", delete_before);
     for row in postgres_client.query("SELECT * FROM job", &[]).await? {
         let id: &str = row.try_get("job_id")?;
@@ -43,6 +77,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("Cleaning up Job ID: {}", id);
             let mut key = "jobs/".to_owned();
             key.push_str(id);
+            key.push_str("/");
             let results = minio_bucket.list(key.clone(), None).await?;
             for result in results {
                 for item in result.contents {
@@ -59,10 +94,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     archive.seek(SeekFrom::Start(0))?;
-    let mut key = "archive/".to_owned();
-    key.push_str(&SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?.as_secs().to_string());
-    key.push_str(".csv");
-    s3_bucket.put_object_stream(& mut archive, key).await?;
+    let mut key = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)?.as_secs().to_string();
+    key.push_str("/archive.csv");
+    archive_bucket.put_object_stream(& mut archive, key).await?;
 
     Ok(())
 }
