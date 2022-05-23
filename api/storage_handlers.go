8307abe2bb6cc3643181a803980b2eb05e793621
Frantjc <logsdonalex11@gmail.com>
2022-05-22 21:35:35 -0400

    run swag fmt

--- api/storage_handlers.go
+++ api/storage_handlers.go
@@ -9,16 +9,16 @@ import (
 	"github.com/logsquaredn/geocloud"
 )
 
-// @Summary  Get a list of storage
-// @Description Get a list of stored datasets based on API Key
-// @Tags listStorage
-// @Produce  application/json
-// @Param api-key query string false "API Key via query parameter"
-// @Param X-API-Key header string false "API Key via header"
-// @Success  200  {object}  []geocloud.Storage
-// @Failure  401  {object}  geocloud.Error
-// @Failure  500  {object}  geocloud.Error
-// @Router   /storage [get]
+// @Summary      Get a list of storage
+// @Description  Get a list of stored datasets based on API Key
+// @Tags         listStorage
+// @Produce      application/json
+// @Param        api-key    query     string  false  "API Key via query parameter"
+// @Param        X-API-Key  header    string  false  "API Key via header"
+// @Success      200        {object}  []geocloud.Storage
+// @Failure      401        {object}  geocloud.Error
+// @Failure      500        {object}  geocloud.Error
+// @Router       /storage [get]
 func (a *API) listStorageHandler(ctx *gin.Context) {
 	storage, err := a.ds.GetCustomerStorage(a.getAssumedCustomer(ctx))
 	switch {
@@ -33,19 +33,19 @@ func (a *API) listStorageHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, storage)
 }
 
-// @Summary  Get a storage
-// @Description Get the metadata of a stored dataset
-// @Tags getStorage
-// @Produce  application/json
-// @Param api-key query string false "API Key via query parameter"
-// @Param X-API-Key header string false "API Key via header"
-// @Param    id   path      string  true  "Storage ID"
-// @Success  200  {object}  geocloud.Storage
-// @Failure  401  {object}  geocloud.Error
-// @Failure  403  {object}  geocloud.Error
-// @Failure  404  {object}  geocloud.Error
-// @Failure  500  {object}  geocloud.Error
-// @Router   /storage/{id} [get]
+// @Summary      Get a storage
+// @Description  Get the metadata of a stored dataset
+// @Tags         getStorage
+// @Produce      application/json
+// @Param        api-key    query     string  false  "API Key via query parameter"
+// @Param        X-API-Key  header    string  false  "API Key via header"
+// @Param        id         path      string  true   "Storage ID"
+// @Success      200        {object}  geocloud.Storage
+// @Failure      401        {object}  geocloud.Error
+// @Failure      403        {object}  geocloud.Error
+// @Failure      404        {object}  geocloud.Error
+// @Failure      500        {object}  geocloud.Error
+// @Router       /storage/{id} [get]
 func (a *API) getStorageHandler(ctx *gin.Context) {
 	var (
 		storage, statusCode, err = a.getStorage(
@@ -63,21 +63,21 @@ func (a *API) getStorageHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, storage)
 }
 
-// @Summary  Get a storage's content
-// @Description Gets the content of a stored dataset
-// @Tags getStorageContent
-// @Produce  application/json, application/zip
-// @Param Content-Type header string false "Request results as a Zip or JSON. Default Zip"
-// @Param api-key query string false "API Key via query parameter"
-// @Param X-API-Key header string false "API Key via header"
-// @Param    id  path  string  true  "Storage ID"
-// @Success  200
-// @Failure  400  {object}  geocloud.Error
-// @Failure  401  {object}  geocloud.Error
-// @Failure  403  {object}  geocloud.Error
-// @Failure  404  {object}  geocloud.Error
-// @Failure  500  {object}  geocloud.Error
-// @Router   /storage/{id}/content [get]
+// @Summary      Get a storage's content
+// @Description  Gets the content of a stored dataset
+// @Tags         getStorageContent
+// @Produce      application/json, application/zip
+// @Param        Content-Type  header  string  false  "Request results as a Zip or JSON. Default Zip"
+// @Param        api-key       query   string  false  "API Key via query parameter"
+// @Param        X-API-Key     header  string  false  "API Key via header"
+// @Param        id            path    string  true   "Storage ID"
+// @Success      200
+// @Failure      400  {object}  geocloud.Error
+// @Failure      401  {object}  geocloud.Error
+// @Failure      403  {object}  geocloud.Error
+// @Failure      404  {object}  geocloud.Error
+// @Failure      500  {object}  geocloud.Error
+// @Router       /storage/{id}/content [get]
 func (a *API) getStorageContentHandler(ctx *gin.Context) {
 	storage, statusCode, err := a.getStorage(
 		ctx,
@@ -105,20 +105,20 @@ func (a *API) getStorageContentHandler(ctx *gin.Context) {
 	ctx.Data(http.StatusOK, contentType, b)
 }
 
-// @Summary Create a storage
-// @Description Stores a dataset. The ID of this stored dataset can be used as input to jobs
-// @Tags createStorage
-// @Accept application/json, application/zip
-// @Produce application/json
-// @Param name query string false "Human readable name of storage"
-// @Param api-key query string false "API Key via query parameter"
-// @Param X-API-Key header string false "API Key via header"
-// @Param    name  query     string  false  "Storage name"
-// @Success  200   {object}  geocloud.Storage
-// @Failure  400   {object}  geocloud.Error
-// @Failure  401   {object}  geocloud.Error
-// @Failure  500   {object}  geocloud.Error
-// @Router   /storage [post]
+// @Summary      Create a storage
+// @Description  Stores a dataset. The ID of this stored dataset can be used as input to jobs
+// @Tags         createStorage
+// @Accept       application/json, application/zip
+// @Produce      application/json
+// @Param        name       query     string  false  "Human readable name of storage"
+// @Param        api-key    query     string  false  "API Key via query parameter"
+// @Param        X-API-Key  header    string  false  "API Key via header"
+// @Param        name       query     string  false  "Storage name"
+// @Success      200        {object}  geocloud.Storage
+// @Failure      400        {object}  geocloud.Error
+// @Failure      401        {object}  geocloud.Error
+// @Failure      500        {object}  geocloud.Error
+// @Router       /storage [post]
 func (a *API) createStorageHandler(ctx *gin.Context) {
 	storage, statusCode, err := a.createStorage(ctx)
 	if err != nil {
