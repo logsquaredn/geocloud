d0c5889fb71bba87e0520391e99d5fd06e0afd57
phish3y <logsdonevan1@gmail.com>
2022-09-13 18:31:16 -0700

    swag format

--- pkg/service/customer_handlers.go
+++ pkg/service/customer_handlers.go
@@ -12,14 +12,14 @@ type createCustomerQuery struct {
 	email string `form:"email" binding:"required"`
 }
 
-// @Summary      Create a new customer
-// @Tags         Customer
-// @Accept application/json
-// @Produce      application/json
-// @Param email query string true "Customer email"
-// @Success      200  {object}  rototiller.Customer
-// @Failure      500  {object}  api.Error
-// @Router       /api/v1/customers/create [post].
+// @Summary  Create a new customer
+// @Tags     Customer
+// @Accept   application/json
+// @Produce  application/json
+// @Param    email  query     string  true  "Customer email"
+// @Success  200    {object}  rototiller.Customer
+// @Failure  500    {object}  api.Error
+// @Router   /api/v1/customers/create [post].
 func (a *Handler) createCustomerHandler(ctx *gin.Context) {
 	if err := ctx.ShouldBindQuery(&createCustomerQuery{}); err != nil {
 		a.err(ctx, api.NewErr(err, http.StatusBadRequest))
