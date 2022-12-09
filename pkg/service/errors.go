5f048c1e22d38413042e13cd8c10c3265e62bb22
phish3y <logsdonevan1@gmail.com>
2022-12-08 22:02:31 -0800

    make sure all errors in the api are logged

--- pkg/service/errors.go
+++ pkg/service/errors.go
@@ -2,10 +2,14 @@ package service
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/logsquaredn/rototiller"
 	"github.com/logsquaredn/rototiller/pkg/api"
 )
 
 func (a *Handler) err(ctx *gin.Context, err error) {
+	logr := rototiller.LoggerFrom(ctx)
+
 	apiErr := api.NewErr(err)
+	logr.Error(apiErr, "")
 	ctx.JSON(apiErr.HTTPStatusCode, apiErr)
 }
