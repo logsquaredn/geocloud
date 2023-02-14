787f8f46f37b4bf4d52fb3c0d4b6a398bbb645b1
Frantjc <logsdonalex11@gmail.com>
2023-02-14 01:35:55 -0500

    bad sql

--- store/data/postgres/sql/queries/get_jobs_before.sql
+++ store/data/postgres/sql/queries/get_jobs_before.sql
@@ -1,3 +1,3 @@
-SELECT job_id, namespace, input_id, output_id, task_type, job_status, job_error, start_time, end_time, job_args
+SELECT job_id, namespace, input_id, output_id, job_status, job_error, start_time, end_time
 FROM job
 WHERE end_time < $1;
