c5807bf5bb0c2fd71773fa5cf3a8ae40e01f2005
Frantjc <logsdonalex11@gmail.com>
2023-02-14 01:19:30 -0500

    bad merge

--- store/data/postgres/sql/execs/create_job.sql
+++ store/data/postgres/sql/execs/create_job.sql
@@ -2,12 +2,8 @@ INSERT INTO job (
     job_id,
     namespace,
     input_id,
-    task_type,
-    job_args
 ) VALUES (
     $1,
     $2,
-    $3,
-    $4,
-    $5
-) RETURNING job_id, namespace, input_id, output_id, task_type, job_status, job_error, start_time, end_time, job_args;
+    $3
+) RETURNING job_id, namespace, input_id, output_id, job_status, job_error, start_time, end_time;
